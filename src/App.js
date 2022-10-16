539c493f6b270f3b547a679ce43d5dc020c85263
Wesley Jean-Charles <jeanchaw@oregonstate.edu>
2022-10-16 17:28:25 -0400

    version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import fighter from "./pages/fighter_profile";
+import {useState, useEffect} from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+        Welcome to the Olympic Judo Search Website!
+      </h1>
+            <table>
+                <th>
+                    <a href="src/pages/fighter_profile.js"> Fighter Profile</a>
+                </th>
+                <th>
+                    <a href="/pages/olympic_game">Olympic Games</a>
+                </th>
+                <th>
+                    <a href="/pages/country_profile">Country Profile</a>
+                </th>
+            </table>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            <form>
+                <label>
+                    Search For Judo Info:
+                    <input type={"text"} name={"search"}/>
+                </label>
+            </form>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
+
   );
 }
 
